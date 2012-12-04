67eee6c1f404d106647067bd0b11cd12cf040d17
Dry Bones <telrikk@gmail.com>
2012-12-03 20:01:24 -0500

    zips/genders are now validated a bit more thoroughly

--- app-api/controllers/account.js
+++ app-api/controllers/account.js
@@ -1,4 +1,4 @@
-var Account = require('../models/account.js');
+var account = require('../models/account.js');
 var bcrypt = require('bcrypt');
 var config = require('../config.js');
 var crypto = require('crypto');
@@ -13,8 +13,11 @@ exports.post = function(req, res) {
 
 	if (data.birthDate != undefined) { accountData.birthDate = data.birthDate; }
 	if (data.email != undefined) { accountData.email = data.email; }
-	if (data.gender != undefined) { accountData.gender = data.gender; }
-	if (data.zip != undefined) { accountData.zip = data.zip; }
+	if (data.gender != undefined && data.zip.gender.length == 1) {
+		accountData.gender = data.gender;
+	}
+	var validZip = data.zip != undefined && data.zip.length == 5 && data.zip.match('/^[0-9]{5}$/');
+	if (validZip) { accountData.zip = data.zip; }
 	accountData.username = data.username;
 	accountData.password = hashedPassword;
 	accountData.activationKey = activationKey;
@@ -22,15 +25,7 @@ exports.post = function(req, res) {
 	accountData.firstName = data.firstName;
 	accountData.lastName = data.lastName;
 
-	new Account(accountData).save(function(err, account) {
-		if(err) {
-			console.log(err);
-			res.send(409);
-			return;
-		}
-		//TODO: send email with an activation key
-		res.send(201, 'id: ' + account.id + ' username: ' + accountData.username);
-	});
+	account.insertAccount(accountData, res);
 }
 
 exports.put = function(req, res) {
@@ -38,27 +33,16 @@ exports.put = function(req, res) {
 	var conditions = {};
 	var data = req.body;
 
-	if(data.password != undefined) {
+	if (data.password != undefined) {
 		var hashedPassword = bcrypt.hashSync(data.password, config.app.salt);
 		accountData.password = hashedPassword;
 	}
 
 	if (data.email != undefined) { accountData.email = data.email; }
-	if (data.zip != undefined) { accountData.zip = data.zip; }
+	var validZip = data.zip != undefined && data.zip.length == 5 && data.zip.match('/^[0-9]{5}$/');
+	if (validZip) { accountData.zip = data.zip; }
 
 	conditions.username = req.params.username;
 
-	Account.update(conditions, accountData, function(err, numberAffected, rawResponse) {
-		if(err) {
-			console.log(err);
-			res.send(400);
-			return;
-		}
-		if(numberAffected === 0) {
-			console.log('did not update any rows for username ' + req.params.username);
-			res.send(400);
-			return;
-		}
-		res.send(200);
-	})
+	account.updateAccount(accountData, res);
 }
--- app-api/models/account.js
+++ app-api/models/account.js
@@ -14,4 +14,32 @@ var accountSchema = new Schema({
 	zip: {type: String, required: false}
 });
 
-module.exports = mongoose.model('account', accountSchema);
+var AccountModel = mongoose.model('account', accountSchema);
+
+exports.insertAccount = function(accountData, res) {
+	new AccountModel(accountData).save(function(err, newAccount) {
+		if (err) {
+			console.log(err);
+			res.send(409);
+			return;
+		}
+		//TODO: send email with an activation key
+		res.send(201, newAccount);
+	});
+};
+
+exports.updateAccount = function(accountData, res) {
+	AccountModel.update(conditions, accountData, function(err, numberAffected, rawResponse) {
+		if (err) {
+			console.log(err);
+			res.send(400);
+			return;
+		}
+		if (numberAffected === 0) {
+			console.log('did not update any rows for username ' + req.params.username);
+			res.send(400);
+			return;
+		}
+		res.send(200);
+	})
+}
