2ad820ea4809b2729f7e632025dffd25b450d525
Dry Bones <telrikk@gmail.com>
2012-12-10 22:57:04 -0500

    Added tests for modifying accounts

--- app-api/test/account.js
+++ app-api/test/account.js
@@ -1,6 +1,7 @@
 var accounts = require('../models/account.js');
 var assert = require('assert');
 var config = require('../config.js');
+var constants = require('../constants.js');
 var mocha = require('mocha');
 var mongoose = require('mongoose');
 var request = require('request');
@@ -43,7 +44,7 @@ describe('Accounts', function () {
 			request(postRequest, function (error, response, body) {
 				assert(body.activationKey != undefined);
 				assert(body.password != undefined);
-				assert.equal(response.statusCode, '201');
+				assert.equal(response.statusCode, constants.HTTP_CREATED);
 				assert.equal(body.birthDate, '2012-12-12T05:00:00.000Z');
 				assert.equal(body.email, 'test@test.com');
 				assert.equal(body.gender, 'M');
@@ -57,26 +58,26 @@ describe('Accounts', function () {
 
 		it('Should not add unrecognized fields to the database when creating an account',
 				function (done) {
-					var testData = {
-						email:'test@test.com',
-						firstName:'I am a',
-						invalid: 'invalid',
-						lastName:'test',
-						password:'test',
-						username:'test'
-					};
+			var testData = {
+				email:'test@test.com',
+				firstName:'I am a',
+				invalid: 'invalid',
+				lastName:'test',
+				password:'test',
+				username:'test'
+			};
 
-					var postRequest = {
-						json:testData,
-						uri:'http://localhost:3000/users',
-						method:'POST'
-					};
+			var postRequest = {
+				json:testData,
+				uri:'http://localhost:3000/users',
+				method:'POST'
+			};
 
-					request(postRequest, function (error, response, body) {
-						assert(body.invalid == undefined);
-						done();
-					});
-				});
+			request(postRequest, function (error, response, body) {
+				assert(body.invalid == undefined);
+				done();
+			});
+		});
 
 		it('Should not create a user if the username is already taken.', function (done) {
 			var testData = {
@@ -95,9 +96,9 @@ describe('Accounts', function () {
 
 			request(postRequest, function (error, response, body) {
 				assert.equal(body.username, 'test');
-				assert.equal(response.statusCode, 201);
+				assert.equal(response.statusCode, constants.HTTP_CREATED);
 				request(postRequest, function (error, response, body) {
-					assert.equal(response.statusCode, 409);
+					assert.equal(response.statusCode, constants.HTTP_CONFLICT);
 					done();
 				});
 			});
@@ -112,16 +113,20 @@ describe('Accounts', function () {
 			var testData = {
 				activationKey: 'testAct',
 				apiKey : 'testKey',
+				birthDate: '12/12/12',
 				email:'test@test.com',
+				gender: 'M',
 				firstName:'I am a',
 				lastName:'test',
 				password:'test',
-				username:'test'
+				username:'test',
+				zip: '19301'
 			};
 
 			new accounts.AccountModel(testData).save(function(err, newAccount) {
 				id = newAccount._id;
 				password = newAccount.password;
+				console.log(newAccount.apiKey);
 				done();
 			});
 		});
@@ -134,7 +139,7 @@ describe('Accounts', function () {
 
 		it('Should update all valid fields on an account', function(done) {
 			var testData = {
-				apiKey : 'apiKey',
+				apiKey : 'testKey',
 				password:'testChanged',
 				email:'test@test.comChanged',
 				zip: '19304'
@@ -147,9 +152,8 @@ describe('Accounts', function () {
 			};
 
 			request(putRequest, function (error, response, body) {
-				assert.equal(response.statusCode, '200');
+				assert.equal(response.statusCode, constants.HTTP_OK);
 				accounts.AccountModel.findById(id, function(err, account) {
-					console.log('thinger' + account);
 					assert.equal(account.email, 'test@test.comChanged');
 					assert.equal(account.zip, '19304');
 					assert.notEqual(account.password, password);
@@ -159,16 +163,71 @@ describe('Accounts', function () {
 		});
 
 		it('Should not update an account with an incorrect API key', function(done) {
-			done();
+			var testData = {
+				apiKey : 'failKey'
+			};
+
+			var putRequest = {
+				json:testData,
+				uri:'http://localhost:3000/users/' + id,
+				method:'PUT'
+			};
+
+			request(putRequest, function (error, response, body) {
+				assert.equal(response.statusCode, constants.HTTP_UNAUTHORIZED);
+				done();
+			});
 		});
 
 		it('Should not update any unrecognized/not writable fields on an account',
 				function(done) {
-			done();
+			var testData = {
+				apiKey: 'testKey',
+				birthDate: 'Modified',
+				creationDate: 'Modified',
+				gender: 'Modified',
+				invalid: 'invalid',
+				firstName: 'Modified',
+				lastName: 'Modified',
+				username: 'Modified'
+			};
+
+			var putRequest = {
+				json:testData,
+				uri:'http://localhost:3000/users/' + id,
+				method:'PUT'
+			};
+
+			request(putRequest, function (error, response, body) {
+				assert.equal(response.statusCode, constants.HTTP_OK);
+				accounts.AccountModel.findById(id, function(err, account) {
+					assert.notEqual(account.birthDate, 'Modified');
+					assert.notEqual(account.creationDate, 'Modified');
+					assert.notEqual(account.gender, 'Modified');
+					assert.notEqual(account.firstName, 'Modified');
+					assert.notEqual(account.lastName, 'Modified');
+					assert.notEqual(account.userName, 'Modified');
+					assert(account.invalid == undefined);
+					done();
+				});
+			});
 		});
 
 		it('Should not update an account that doesn\'t exist', function(done) {
-			done();
+			var testData = {
+				apiKey : 'testKey'
+			};
+
+			var putRequest = {
+				json:testData,
+				uri:'http://localhost:3000/users/notarealid',
+				method:'PUT'
+			};
+
+			request(putRequest, function (error, response, body) {
+				assert.equal(response.statusCode, constants.HTTP_BADREQUEST);
+				done();
+			});
 		});
 	});
 });
